8f4fe2bebbdaa055955b57cf9e9851568f4ab0c2
sahanashettigar <r.sahana.shettigar@gmail.com>
2021-10-31 16:57:22 +0530

    added last simple query

--- apna_ghar_queries.sql
+++ apna_ghar_queries.sql
@@ -7,8 +7,6 @@ select fname,lname,pet_info from resident where pet_info!='no pet';
 select re.flatid,fname,lname from flat as f,resident as r,resident_residesin_flat as re where (f.flatid=re.FlatID and r.aadhar=re.residentuid and no_bhk>2);
 -- select resident flatid,fname,lname who OWN flat
 select Fname,Lname,f.FlatID from resident as r,flat as f, resident_residesin_flat as re where r.Aadhar=re.residentUID and re.FlatID=f.FlatID and rent_owned='Own';
--- select resident flatid,fname,lname who rented flat
-select Fname,Lname from resident as r,resident_residesin_flat as re where r.Aadhar=re.residentUID and re.rent_owned='Rent';
 --display resident's firstname,last name and number of complaints made so far
 select r.Fname,r.Lname,(select count(*) from complaints where ResidentUID=r.Aadhar) as no_of_complaints from resident as r;
 -- select securityID and their name who guard Flat's in A block and are on night shift
@@ -19,3 +17,5 @@ select serviceID from services where cost<500 UNION select serviceID from employ
 select Fname,Lname,shift,gender,type,cost from employee as e INNER JOIN services as s ON e.serviceID = s.serviceID;
 --display residents name and the number of dependents whose age is more 60
 select Fname,Lname,(select count(*) from depenedent where residentUID=r.Aadhar and (EXTRACT(year FROM age(now(),DOB)) :: int)>60) from resident as r where (select count(*) from depenedent where residentUID=r.Aadhar and (EXTRACT(year FROM age(now(),DOB)) :: int)>60)>0 ;
+--display name and the service availed by the resident
+select Fname,Lname,rrf.FlatID,type as service_availed from resident_avails_services as ras,resident as r,services as s,flat as f,resident_residesin_flat as rrf WHERE rrf.FlatID=f.FlatID AND rrf.ResidentUID=r.Aadhar AND ras.ResidentUID=r.Aadhar AND ras.serviceID=s.serviceID;
