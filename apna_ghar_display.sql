2dbc2222a76772eebe915a1e0f6ff0f3b50a03ac
sahanashettigar <r.sahana.shettigar@gmail.com>
2021-10-31 11:22:23 +0530

    complex queries

--- apna_ghar_display.sql
+++ apna_ghar_display.sql
@@ -1,9 +1,10 @@
+\c apna_ghar
 select * from resident;
 select * from security;
 select * from employee;
 select * from flat;
 select * from parking_slot;
-SELECT * FROM dependent;
+SELECT * FROM depenedent;
 SELECT * FROM visitor;
 SELECT * FROM complaints;
 SELECT * FROM resident_residesin_flat;
