8bef411a5ead0fdc02cb674d46c82dd860aa8431
Ritesh Sapata <riteshsapata@gmail.com>
2021-11-03 17:13:11 +0530

    Rewrote the users sql file

--- apna_ghar_users.sql
+++ apna_ghar_users.sql
@@ -1,30 +1,43 @@
+-- Dropping users if they exist
+drop user admin;
+drop user resident;
+drop user security;
+
 -- Creating the different users
-create user admin with password 'password1' apna_ghar;
-create user resident with password 'password2' apna_ghar;
-create user security with password 'password3' apna_ghar;
+create user admin with password 'password1' nocreatedb;
+create user resident with password 'password2' nocreatedb;
+create user security with password 'password3' nocreatedb;
 
--- Read permissions to different users
-grant select on 
-resident, security, services, employee, flat, parking_slot, dependent, visitor, complaints, resident_residesin_flat, flat_has_security, resident_avails_services
-to admin;
+-- Select, Insert and Update permissions to admin
+grant select,insert,update on resident to admin;
+grant select,insert,update on security to admin;
+grant select,insert,update on services to admin;
+grant select,insert,update on employee to admin;
+grant select,insert,update on flat to admin;
+grant select,insert,update on parking_slot to admin;
+grant select,insert,update on dependent to admin;
+grant select,insert,update on visitor to admin;
+grant select,insert,update on complaints to admin;
+grant select,insert,update on flat_has_security to admin;
+grant select,insert,update on resident_residesin_flat to admin;
+grant select,insert,update on resident_avails_services to admin;
 
-grant select on 
-resident, services, dependent, visitor, complaints, flat_has_security, security, resident_avails_services,
-to resident;
+-- Select, Insert and Update permissions to resident
+grant select,update on resident to resident;
+grant select on services to resident;
+grant select,update on dependent to resident;
+grant select on visitor to resident;
+grant select,insert on complaints to resident;
+grant select on flat_has_security to resident;
+grant select on security to resident;
+grant select,insert on resident_avails_services to resident;
 
-grant select on 
-flat_has_security,resident_residesin_flat, visitor, dependent, parking_slot, flat, resident
-to security;
+-- Select and Insert permissions to security
+grant select on flat_has_security to security;
+grant select on resident_residesin_flat to security;
+grant select,insert on visitor to security;
+grant select on dependent to security;
+grant select on parking_slot to security;
+grant select on flat to security;
+grant select on resident to security;
 
--- Write permissions to different users
-grant insert on 
-resident, security, services, employee, flat, parking_slot, dependent, visitor, complaints, resident_residesin_flat, flat_has_security, resident_avails_services
-to admin;
-
-grant insert on 
-resident, services, complaints, resident_avails_services,
-to resident;
-
-grant insert on 
-visitor
-to security;
