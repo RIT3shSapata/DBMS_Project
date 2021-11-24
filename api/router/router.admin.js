c356021d4150adb1f652d11d654c659b203f3b28
sachinvivek <sachinvivek@gmail.com>
2021-11-24 15:53:54 +0530

    Update router.admin.js

--- api/router/router.admin.js
+++ api/router/router.admin.js
@@ -4,6 +4,9 @@ const {
     addService,
     addEmployee,
     viewResidentInfo,
+    viewComplaints,
+    viewEmployees,
+    viewSecurity,
 } = require('../controllers/controller.admin');
 
 const router = require('express').Router();
@@ -13,4 +16,7 @@ router.post('/security', addSecurity);
 router.post('/service', addService);
 router.post('/employee', addEmployee);
 router.get('/residentinfo', viewResidentInfo);
+router.get('/complaint_info', viewComplaints);
+router.get('/employee_info', viewEmployees);
+router.get('/security_info', viewSecurity);
 module.exports = router;
