dbabef65d719a7a0464417d8f900d97258249803
sahanashettigar <r.sahana.shettigar@gmail.com>
2021-11-24 16:42:22 +0530

    axios for viewing resident info

--- frontend/src/components/admin/tables/viewResidentFlat.jsx
+++ frontend/src/components/admin/tables/viewResidentFlat.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -7,14 +7,27 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Table from '@mui/material/Table';
 import Paper from '@mui/material/Paper';
+import axios from '../../../axios';
 export default function ViewResidents() {
-    
-  function createDataVisitors(residentuid,flatuid, rent_owned,visitor_count) {
-    return { residentuid,flatuid, rent_owned,visitor_count };
-  }
-  const rows_visitors = [
-    createDataVisitors('12345','12345','rent','0')
-  ];
+  
+  const [residenttable,setResidentTable] = useState([]);
+  var rows_visitors = []
+  useEffect(() => {
+    const getresidentdata=async()=>{
+      try{
+        const response = await axios.get('/admin/residentinfo');
+        setResidentTable(response.data);
+    //     //console.log(residenttable)
+    //     for (let index = 0; index < residenttable.length; index++) {
+    // const element = residenttable[index];
+    // row_visitors.push(createDataVisitors(element.residentuid,element.flatid,element.rent_owned,element.count));
+        }
+      catch(e){
+        console.log(e)
+      }
+    }
+    getresidentdata() 
+  }, [])
     return (
     <div>
     <h1 className="text-2xl flex justify-center">Residents and their flat information</h1>
@@ -29,14 +42,14 @@ export default function ViewResidents() {
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows_visitors.map((row) => (
+          {residenttable.map((row) => (
             <TableRow>
               <TableCell component="th" scope="row">
                 {row.residentuid}
               </TableCell>
-              <TableCell align="right">{row.flatuid}</TableCell>
+              <TableCell align="right">{row.flatid}</TableCell>
               <TableCell align="right">{row.rent_owned}</TableCell>
-              <TableCell align="right">{row.visitor_count}</TableCell>
+              <TableCell align="right">{row.count}</TableCell>
             </TableRow>
           ))}
         </TableBody>
